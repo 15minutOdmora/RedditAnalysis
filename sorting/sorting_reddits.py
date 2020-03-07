8828439b7cbf46c8a005bdee0242362cf48a17b6
AliGremLahkoNaWC <44203228+AliGremLahkoNaWC@users.noreply.github.com>
2020-03-07 19:05:22 +0100

    Update sorting_reddits.py

--- sorting/sorting_reddits.py
+++ sorting/sorting_reddits.py
@@ -14,9 +14,9 @@ for line in open('subredditi.txt', 'r'):
     if num == 1:
         num = 0
     elif line[1].strip('/r/') in nsfw:
-        list_reddit['nsfw'] += [line[1].strip('/r/'), int(line[0].replace(',','')), int(line[2].replace(',',''))]
+        list_reddit['nsfw'] += [[line[1].strip('/r/'), int(line[0].replace(',','')), int(line[2].replace(',',''))]]
     else:
-        list_reddit['normal'] += [line[1].strip('/r/'), int(line[0].replace(',','')), int(line[2].replace(',',''))]
+        list_reddit['normal'] += [[line[1].strip('/r/'), int(line[0].replace(',','')), int(line[2].replace(',',''))]]
 
 with open('reddit_dict1.json', 'w') as fp:
     json.dump(list_reddit, fp, indent = 4)
