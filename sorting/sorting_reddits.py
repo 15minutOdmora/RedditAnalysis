d5d058e0221d4b54f1fdbc83c88beccf3594ea2b
AliGremLahkoNaWC <44203228+AliGremLahkoNaWC@users.noreply.github.com>
2020-03-08 15:47:27 +0100

    Update sorting_reddits.py

--- sorting/sorting_reddits.py
+++ sorting/sorting_reddits.py
@@ -1,8 +1,12 @@
 import json
+import praw
 
 list_reddit = {'nsfw': [], 'normal': [], 'usa': [], 'europe': []}
 nsfw = []
 num = 1
+reddit = praw.Reddit(client_id='n1WIc796crv1mQ',
+                     client_secret='l2ZeRpYtZLl8gOQp-xILlOXiGHM',
+                     user_agent='RedditAnalysis2')
 
 for line in open('nsfw.txt', 'r'):
     line = line.split()
@@ -11,12 +15,21 @@ for line in open('nsfw.txt', 'r'):
 
 for line in open('subredditi.txt', 'r'):
     line = line.split()
+    name, rank, subs = line[1].replace('/r/',''), int(line[0].replace(',','')), int(line[2].replace(',',''))
+    #print(name, rank, subs)
+    sub = reddit.subreddit(name)
+    #print(sub)
     if num == 1:
         num = 0
-    elif line[1].strip('/r/') in nsfw:
-        list_reddit['nsfw'] += [[line[1].strip('/r/'), int(line[0].replace(',','')), int(line[2].replace(',',''))]]
     else:
-        list_reddit['normal'] += [[line[1].strip('/r/'), int(line[0].replace(',','')), int(line[2].replace(',',''))]]
+        try:
+            if sub.title:
+                if name in nsfw:
+                    list_reddit['nsfw'] += [[name, rank, subs]]
+                else:
+                    list_reddit['normal'] += [[name, rank, subs]]
+        except:
+            pass
 
-with open('reddit_dict1.json', 'w') as fp:
+with open('reddit_dict88.json', 'w') as fp:
     json.dump(list_reddit, fp, indent = 4)
